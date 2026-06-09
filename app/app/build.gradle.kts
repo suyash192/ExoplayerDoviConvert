7d7a64fdf90d043e76e436acf0c84289e8faf980
Suyash <suyash192@gmail.com>
2026-06-09 20:17:19 +0530

    Add maven publish plugin.

--- app/app/build.gradle.kts
+++ app/app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.library)
+    alias(libs.plugins.maven.publish)
 }
 
 version = "0.1.0"
@@ -7,11 +8,7 @@ group = "com.suyashbelekar.exoplayerhdrutils"
 
 android {
     namespace = "com.suyashbelekar.exoplayerhdrutils"
-    compileSdk {
-        version = release(37) {
-            minorApiLevel = 0
-        }
-    }
+    compileSdk = 36
 
     defaultConfig {
         minSdk = 23
@@ -32,4 +29,37 @@ dependencies {
     implementation(libs.androidx.core.ktx)
 
     implementation(libs.androidx.media3.exoplayer)
+}
+
+mavenPublishing {
+    coordinates("com.suyashbelekar", "exoplayerhdrutils", "0.1.0")
+
+    pom {
+        name.set("ExoPlayer HDR Utils")
+        description.set("ExoPlayer utilities for Dolby Vision and HDR10+ metadata conversions using libdovi.")
+        inceptionYear.set("2026")
+        url.set("https://github.com/suyash192/ExoplayerHdrUtils")
+        licenses {
+            license {
+                name.set("MIT License")
+                url.set("https://opensource.org/licenses/MIT")
+            }
+        }
+        developers {
+            developer {
+                id.set("suyash192")
+                name.set("Suyash Belekar")
+                url.set("https://github.com/suyash192/")
+            }
+        }
+        scm {
+            url.set("https://github.com/suyash192/ExoplayerHdrUtils/")
+            connection.set("scm:git:git://github.com/suyash192/ExoplayerHdrUtils.git")
+            developerConnection.set("scm:git:ssh://git@github.com/suyash192/ExoplayerHdrUtils.git")
+        }
+    }
+
+    publishToMavenCentral()
+
+    signAllPublications()
 }
